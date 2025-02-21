3d484a3e44397258ac82fc88580ea57272a4499c
Stephen <sirugh@users.noreply.github.com>
2025-02-21 08:26:14 -0600

    Update L322-initialize.sh
    fix org selection condition.
    use latest mesh
    set telemetry on

--- L322-initialize.sh
+++ L322-initialize.sh
@@ -7,7 +7,7 @@ gh auth login --with-token < ~/.git-token
 
 # install aio cli and ensure correct aio env used, and login
 npm install -g @adobe/aio-cli
-aio telemetry off
+aio telemetry on
 aio config set cli.env prod
 # perform login, should automatically authenticate since lab machines are being pre-authenticated to experience.adobe.com (prod IMS) on browser.
 aio login
@@ -17,7 +17,7 @@ aio login
 aio console org select ${ORG_ID}
 
 # install the mesh and commerce plugins
-aio plugins:install @adobe/aio-cli-plugin-api-mesh@4.1.0-beta.3
+aio plugins:install @adobe/aio-cli-plugin-api-mesh
 aio plugins:install https://github.com/adobe-commerce/aio-cli-plugin-commerce
 
 echo "Verifying installation..."
@@ -36,8 +36,8 @@ if ! aio plugins | grep '@adobe/aio-cli-plugin-commerce' > /dev/null; then
   exit 1
 fi
 
-if ! aio config get console.org | grep '1244026' > /dev/null; then
-  echo "Incorrect ORG selected. Please run 'aio console org select 1244026'"
+if ! aio config get console.org | grep '3117813' > /dev/null; then
+  echo "Incorrect ORG selected. Please run 'aio console org select 3117813'"
   exit 1
 fi
 
